37d15cf6dfd661c5c1acd41f7301a0a2d89627a8
tarun1git <tarunrajput7959@gmail.com>
2022-10-04 01:20:40 +0530

    js chng

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-(function () {
+function Calc(){
+    var val1 = document.getElementById("val1").value;
+    var val2 = document.getElementById("val2").value;
+    var opt = document.getElementById("opt").value;
+    
+    var ans = eval(val1 + opt + val2);
+    document.getElementById("ans").value = ans;
+}
+// (function () {
 
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let count = document.getElementById('.counter');
+//     let screen = document.querySelector('.screen');
+//     let buttons = document.querySelectorAll('.btn');
+//     let count = document.getElementById('.counter');
 
-    screen.value = "value"
+//     screen.value = "value"
 
-    buttons.forEach(function (button) {
-        button.addEventListner('click', function (e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
+//     buttons.forEach(function (button) {
+//         button.addEventListner('click', function (e) {
+//             let value = e.target.dataset.num;
+//             screen.value += value;
 
-            if (screen.value === '') {
-                screen.value = answer;
-            }
-            else {
-                let answer = eval(count);
-                screen.value = answer;
-            }
-        });
-    })
-})
+//             if (screen.value === '') {
+//                 screen.value = answer;
+//             }
+//             else {
+//                 let answer = eval(count);
+//                 screen.value = answer;
+//             }
+//         });
+//     })
+// })
